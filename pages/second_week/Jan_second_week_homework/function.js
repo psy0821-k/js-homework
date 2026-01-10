60213aa5ef1cada8cb0ff9b310ee1bb253a9b582
psy0821-k <p49417875@gmail.com>
2026-01-10 19:05:34 +0900

    함수과제 1번 웰컴 메세지 출력

--- pages/second_week/Jan_second_week_homework/function.js
+++ pages/second_week/Jan_second_week_homework/function.js
@@ -24,3 +24,18 @@ console.log(spiderMan.userRank);
 console.log(guest);
 
 console.log("--------------------------------------------------------------")
+
+// 클린코드에는 정답이 없지만 else if 문을 쓰는것 보다 if문을 쓰는게 
+// 만약 else if문이 많아지면 switch case문을 사용하는 것이 더 명시적이라는 영상을 보았습니다.
+// 선생님의 의견이 궁급합니다.
+
+function userWelcomeMessage (userName, userRank){
+  if( userRank === "vip"){ return "🌟 VIP " + userName + "님, 특별한 혜택이 준비되어 있습니다!" + typeof(userName); }
+  if( userName === undefined){return "비정상적인 접근방식 입니다"}
+  return "안녕하세요, " + userName + "님! 즐거운 쇼핑 되세요." + typeof(userName);
+}
+
+console.log(userWelcomeMessage(ironMan.userName, ironMan.userRank));
+console.log(userWelcomeMessage(batman.userName, batman.userRank));
+console.log(userWelcomeMessage(spiderMan.userName, spiderMan.userRank));
+console.log(userWelcomeMessage(guest.userName, guest.userRank));
