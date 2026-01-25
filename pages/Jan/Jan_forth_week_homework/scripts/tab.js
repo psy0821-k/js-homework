74b5e08dbdc07ba87c0b846e69f5ff7ce83f37a9
psy0821-k <p49417875@gmail.com>
2026-01-25 17:07:34 +0900

    탭 정보 들어가는 영역 제작 탭 네비게이션 영역 문법 검사후 수정

--- pages/Jan/Jan_forth_week_homework/scripts/tab.js
+++ pages/Jan/Jan_forth_week_homework/scripts/tab.js
@@ -1,10 +1,14 @@
 const tabHeader = document.getElementById('tab-header');
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll('button');
 
+const tabContentContainer = document.querySelector('.tab-content-container');
+const tabContentTitle = tabContentContainer.querySelector('.tab-content-title');
 for (const button of buttons) {
   button.addEventListener('click', () => {
     resetButton();
     button.classList.add('active');
+    tabContentTitle.textContent = button.dataset.title;
+    console.log(button.dataset.title);
   });
 }
 
